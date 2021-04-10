f04bb19de9f527ac012530d0b3d44bf4292785eb
99003734 <78858575+99003734@users.noreply.github.com>
2021-04-10 15:19:10 +0530

    Update step1.c

--- HANDS_ON/Psuedo_Drivers/Register_Char_Driver/step1.c
+++ HANDS_ON/Psuedo_Drivers/Register_Char_Driver/step1.c
@@ -25,5 +25,5 @@ static void __exit psuedo_exit(void) {
 module_init(psuedo_init);
 module_exit(psuedo_exit);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Your name");
+MODULE_AUTHOR("Akbar");
 MODULE_DESCRIPTION("A Simple Module");
